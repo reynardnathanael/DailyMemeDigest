fb0f256b7bff0ee22b16daee11212a1150a26bd4
Anri20 <88647934+Anri20@users.noreply.github.com>
2022-12-16 16:46:38 +0700

    navbar blom slese

--- project/app/src/main/java/com/reynard/dailymemedigest/MainActivity.kt
+++ project/app/src/main/java/com/reynard/dailymemedigest/MainActivity.kt
@@ -2,10 +2,22 @@ package com.reynard.dailymemedigest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+//    bottom navbar
+    val fragments: Array<Fragment> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragments.add(HomeFragment())
+        fragments.add(MyCreationFragment())
+        fragments.add(LeaderboardFragment())
+        fragments.add(SettingsFragment())
+
+        viewPager.adapter = MyAdapter(this, fragments)
     }
 }
---
+++ project/app/src/main/java/com/reynard/dailymemedigest/MyAdapter.kt
@@ -0,0 +1,17 @@
+package com.reynard.dailymemedigest
+
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MyAdapter(activity: AppCompatActivity, private val fragments: Array<Fragment>): FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+
+}
