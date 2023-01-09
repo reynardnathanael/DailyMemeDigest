33056cae534dc3212e2e9d9a72088987f9ba25ce
Anri20 <88647934+Anri20@users.noreply.github.com>
2023-01-09 21:11:22 +0700

    drawer Done

--- project/app/src/main/java/com/reynard/dailymemedigest/MainActivity.kt
+++ project/app/src/main/java/com/reynard/dailymemedigest/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.reynard.dailymemedigest
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.drawer.*
 
 class MainActivity : AppCompatActivity() {
-    //    bottom navbar
+    // drawer
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var drawerToggle: ActionBarDrawerToggle
+
+    //  navbar
     val fragments: ArrayList<Fragment> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer)
+//        setContentView(R.layout.activity_main)
 
+        // bottom Navbar
         fragments.add(HomeFragment())
         fragments.add(MyCreationFragment())
         fragments.add(LeaderboardFragment())
@@ -25,14 +35,14 @@ class MainActivity : AppCompatActivity() {
 
         viewPager.adapter = adapter
 
-        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 bottomNav.selectedItemId = bottomNav.menu.getItem(position).itemId
             }
         })
 
         bottomNav.setOnItemSelectedListener {
-            viewPager.currentItem = when(it.itemId){
+            viewPager.currentItem = when (it.itemId) {
                 R.id.ItemHome -> 0
                 R.id.ItemMyCreation -> 1
                 R.id.ItemLeaderboard -> 2
@@ -40,6 +50,43 @@ class MainActivity : AppCompatActivity() {
                 else -> 0
             }
             true
+        }
+
+        // drawer Navbar
+        drawerLayout = findViewById(R.id.drawer_layout)
+        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        // set supaya ada back button di drawer
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // nav_view ada di drawer
+        // kalau item di nav_view di select akan menuju ke fragment tersebut
+        nav_view.setNavigationItemSelectedListener {
+            viewPager.currentItem = when (it.itemId) {
+                R.id.ItemHome -> 0
+                R.id.ItemMyCreation -> 1
+                R.id.ItemLeaderboard -> 2
+                R.id.ItemSettings -> 3
+                else -> 0
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
         }
     }
 }
